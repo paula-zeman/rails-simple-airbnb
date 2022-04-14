1696683308238cd05c785471b7b2e2876b43ecff
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-04-14 19:05:47 +0200

    wrote controller actions

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,30 +1,40 @@
 class FlatsController < ApplicationController
+  before_action :find_flat, only: [:show, :edit, :update, :destroy]
   def index
-
+    @flats = Flat.all
   end
 
   def show
-
   end
 
   def new
-
+    @flat = Flat.new
   end
 
   def create
-
+    @flat = Flat.new(flat_params)
+    flat.save
+    #validation missing
   end
 
   def edit
-
   end
 
   def update
-
+    @flat.update(flat_params)
   end
 
   def destroy
 
   end
 
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
 end
