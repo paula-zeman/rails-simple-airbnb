3758d82be937f6ac77eca3b1d9a158a620e6b3a3
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-04-14 19:35:25 +0200

    created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning up database"
+Flat.destroy_all
+puts "database clean"
+puts "create flats"
+
+20.times do
+  Flat.create(
+    name: Faker::Address.street_name,
+    address: Faker::Address.street_address,
+    description: Faker::Hipster.sentence,
+    price_per_night: rand(15..99),
+    number_of_guests: rand(1..10),
+  )
+end
+puts "done"
